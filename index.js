60e212adf4a54abb1f4e3f0a15272400af8144f3
furuya666 <furuyagary@gmail.com>
2022-10-29 20:11:22 -0400

    Creando el primer servidor en express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Primer proyecrto");
+//import  de express
+import express from 'express';
+//instancia de express
+const app = express()
+//varuable dl puerto
+const port = 3000
+//ruta de bienvenida
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+//en el puerto q corre la app
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
